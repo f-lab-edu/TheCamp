208af4eb9797bd23bec38389896317bae50d5e58
jinyeanseok <wdtcwek11@naver.com>
2023-10-10 19:23:08 +0900

    build.grade 수정

---
+++ app/src/main/java/com/reservation/camping/CampingApiApplication.java
@@ -0,0 +1,8 @@
+package src.main.java.com.reservation.camping;
+
+@SpringBootApplication
+public class CampingApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CampingApiApplication.class, args);
+    }
+}
