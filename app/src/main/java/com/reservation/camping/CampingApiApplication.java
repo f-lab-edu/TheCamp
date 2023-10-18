36936b45baa1ad593d8617815c5594e39347dd30
jinyeanseok <49624922+jinyeanseok@users.noreply.github.com>
2023-10-18 16:34:53 +0900

    Gradle을 활용한 멀티모듈 프로젝트 구성 (#4)
    Co-authored-by: jinyeonseok <jinyeonseok@gmail.com>

---
+++ app/src/main/java/com/reservation/camping/CampingApiApplication.java
@@ -0,0 +1,11 @@
+package com.reservation.camping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CampingApiApplication.class, args);
+    }
+}
