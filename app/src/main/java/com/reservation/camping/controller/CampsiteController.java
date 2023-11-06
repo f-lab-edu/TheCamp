61bc9c0f61da5c458090411fd51d7748076bb1fe
jinyeanseok <jinyeonseok@gmail.com>
2023-11-06 17:36:21 +0900

    - 사용하지 않는 소스 제거

--- app/src/main/java/com/reservation/camping/controller/CampsiteController.java
+++ app/src/main/java/com/reservation/camping/controller/CampsiteController.java
@@ -1,22 +1,15 @@
 package com.reservation.camping.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.reservation.camping.entity.AddressInfo;
 import com.reservation.camping.entity.CampsiteInfo;
 import com.reservation.camping.entity.ReservationInfo;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/booking")
 public class CampsiteController {
 
