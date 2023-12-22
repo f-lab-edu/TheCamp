4893e9a2350455e82f89bfe825938bdf0ee74294
jinyeanseok <wdtcwek11@naver.com>
2023-12-23 00:41:11 +0900

    - updateCampsite service TEST

--- app/src/test/java/com/reservation/camping/service/CampsiteServiceTest.java
+++ app/src/test/java/com/reservation/camping/service/CampsiteServiceTest.java
@@ -41,9 +41,41 @@ class CampsiteServiceTest {
         verify(testDb, times(1)).put(any(), any());
     }
 
-    // ... (other test methods)
+    @Test
+    void updateCampsite() {
+        // Given
+        CampsiteReservationDto campsiteReservationDto = createCampsiteReservationDto();
+
+        // When
+        when(testDb.put(anyLong(), any())).thenReturn(campsiteReservationDto);
+
+        CampsiteInfo createCampsiteInfo = campsiteService.addCampsite(campsiteReservationDto);
+        createCampsiteInfo.getReservationInfo().setCampsiteName("영월 법흥계곡얼음골펜션 UPDATE TEST~~");
+
+        // service에 있는 testDb와 호환되지 않는 것 같다..
+        when(campsiteService.updateCampsite(0L, updateCampsiteReservationDto())).thenReturn(createCampsiteInfo);
+
+        CampsiteInfo updateCampsiteInfo = campsiteService.updateCampsite(0L, campsiteReservationDto);
+
+        assertEquals(campsiteReservationDto.getCampsiteName(), updateCampsiteInfo.getReservationInfo().getCampsiteName());
+
+        verify(testDb, times(1)).put(any(), any());
+
+    }
 
     private CampsiteReservationDto createCampsiteReservationDto() {
+        return CampsiteReservationDto.builder() // add dto
+                .addressName("강원도 영월군 무릉도원면 무릉법흥로 1078-9")
+                .region1DepthName("강원도")
+                .region2DepthName("영월군")
+                .campsiteName("영월 법흥계곡얼음골펜션")
+                .priceRange("40000-40000")
+                .telephone("033-1111-2222")
+                .description("법흥계곡에 위치한 아름다운 추억이 함께하는곳 계곡과 숲을 만끽할수있는 얼음골펜션입니다")
+                .build();
+    }
+
+    private CampsiteReservationDto updateCampsiteReservationDto() {
         return CampsiteReservationDto.builder() // add dto
                 .addressName("강원도 영월군 무릉도원면 무릉법흥로 1078-9")
                 .region1DepthName("강원도")
