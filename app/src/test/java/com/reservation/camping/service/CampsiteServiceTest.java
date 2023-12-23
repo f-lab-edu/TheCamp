bb4a0c5719f3cbc388447048d59e413df26f035e
jinyeanseok <jinyeonseok@gmail.com>
2023-12-23 22:50:09 +0900

    - updateCampsite service TEST 완료지만 검증이 필요할듯..

--- app/src/test/java/com/reservation/camping/service/CampsiteServiceTest.java
+++ app/src/test/java/com/reservation/camping/service/CampsiteServiceTest.java
@@ -42,25 +42,74 @@ class CampsiteServiceTest {
     }
 
     @Test
-    void updateCampsite() {
+    void updateCampsite() { // 성공
         // Given
-        CampsiteReservationDto campsiteReservationDto = createCampsiteReservationDto();
+        CampsiteReservationDto createCampsiteReservationDto = createCampsiteReservationDto();
+        CampsiteReservationDto updateCampsiteReservationDto = updateCampsiteReservationDto();
+
+        when(testDb.get(anyLong())).thenReturn(createCampsiteReservationDto);
 
         // When
-        when(testDb.put(anyLong(), any())).thenReturn(campsiteReservationDto);
+        CampsiteInfo updatedCampsiteInfo = campsiteService.updateCampsite(createCampsiteReservationDto.getReservationId(), updateCampsiteReservationDto);
 
-        CampsiteInfo createCampsiteInfo = campsiteService.addCampsite(campsiteReservationDto);
+        System.out.println("test :" + updatedCampsiteInfo.getReservationInfo().getCampsiteName() + updateCampsiteReservationDto.getCampsiteName());
+
+        assertNotNull(updatedCampsiteInfo);
+        assertEquals(updateCampsiteReservationDto.getCampsiteName(), updatedCampsiteInfo.getReservationInfo().getCampsiteName());
+        verify(testDb, times(1)).put(any(), any());
+    }
+
+//    @Test
+//    void updateCampsite() { // 해당 방법은 실패
+//        // Given
+//        CampsiteReservationDto createCampsiteReservationDto = createCampsiteReservationDto();
+//        CampsiteReservationDto updateCampsiteReservationDto = updateCampsiteReservationDto();
+//
+//        // When
+//        when(testDb.put(anyLong(), any())).thenReturn(createCampsiteReservationDto);
+//        when(testDb.get(anyLong())).thenReturn(createCampsiteReservationDto); // Mock 객체에 get 메서드에 대한 동작을 설정
+//
+//        CampsiteInfo createCampsiteInfo = campsiteService.addCampsite(createCampsiteReservationDto);
+//        createCampsiteInfo.getReservationInfo().setCampsiteName("영월 법흥계곡얼음골펜션 UPDATE TEST~~");
+//
+//        // 테스트 코드에서 Mock 객체에 예상되는 동작을 설정
+//        when(campsiteService.updateCampsite(anyLong(), any())).thenReturn(createCampsiteInfo);
+//
+//    }
+
+//    @Test
+//    void updateCampsite() {
+//        // Given
+//        CampsiteReservationDto createCampsiteReservationDto = createCampsiteReservationDto();
+//        CampsiteReservationDto updateCampsiteReservationDto = updateCampsiteReservationDto();
+//
+//        // When
+//        when(testDb.put(anyLong(), any())).thenReturn(createCampsiteReservationDto);
+//
+//        CampsiteInfo createCampsiteInfo = campsiteService.addCampsite(createCampsiteReservationDto);
+//        createCampsiteInfo.getReservationInfo().setCampsiteName("영월 법흥계곡얼음골펜션 UPDATE TEST~~");
+//
+//        // 테스트 코드에서 null을 전달
+//        when(campsiteService.updateCampsite(anyLong(), updateCampsiteReservationDto)).thenReturn(createCampsiteInfo);
+//    }
+
+    @Test
+    void updateCampsite1() {
+        // Given
+        CampsiteReservationDto createCampsiteReservationDto = createCampsiteReservationDto();
+        CampsiteReservationDto updateCampsiteReservationDto = updateCampsiteReservationDto();
+
+        // When
+        when(testDb.get(anyLong())).thenReturn(createCampsiteReservationDto);
+
+        CampsiteInfo createCampsiteInfo = campsiteService.addCampsite(createCampsiteReservationDto);
         createCampsiteInfo.getReservationInfo().setCampsiteName("영월 법흥계곡얼음골펜션 UPDATE TEST~~");
 
-        // service에 있는 testDb와 호환되지 않는 것 같다..
-        when(campsiteService.updateCampsite(0L, updateCampsiteReservationDto())).thenReturn(createCampsiteInfo);
+        // 테스트 코드에서 null을 전달
+        when(campsiteService.updateCampsite(anyLong(), updateCampsiteReservationDto)).thenReturn(createCampsiteInfo);
 
-        CampsiteInfo updateCampsiteInfo = campsiteService.updateCampsite(0L, campsiteReservationDto);
-
-        assertEquals(campsiteReservationDto.getCampsiteName(), updateCampsiteInfo.getReservationInfo().getCampsiteName());
-
+        assertEquals(createCampsiteInfo.getReservationInfo().getCampsiteName(), updateCampsiteReservationDto.getCampsiteName());
         verify(testDb, times(1)).put(any(), any());
-
     }
 
     private CampsiteReservationDto createCampsiteReservationDto() {
@@ -80,7 +129,7 @@ class CampsiteServiceTest {
                 .addressName("강원도 영월군 무릉도원면 무릉법흥로 1078-9")
                 .region1DepthName("강원도")
                 .region2DepthName("영월군")
-                .campsiteName("영월 법흥계곡얼음골펜션")
+                .campsiteName("영월 법흥계곡얼음골펜션 UPDATE TEST~~")
                 .priceRange("40000-40000")
                 .telephone("033-1111-2222")
                 .description("법흥계곡에 위치한 아름다운 추억이 함께하는곳 계곡과 숲을 만끽할수있는 얼음골펜션입니다")
